8bf65602acd9905535f75eb605d5c6c4aae9ee90
Edith-guru <edithnesa@gmail.com>
2022-04-15 02:11:33 +0300

    add directive, pipe, bootstrap. import bootstrap to root style.css

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Goal } from '../goal';
 
 @Component({
@@ -11,6 +11,17 @@ export class GoalDetailComponent implements OnInit {
  
   @Input()
   goal!: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
