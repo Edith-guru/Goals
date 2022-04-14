5fedc2f65ea91ceb11cd6f327f842ef92da90b64
Edith-guru <edithnesa@gmail.com>
2022-04-14 19:21:21 +0300

    directive loop for goals in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goal = 'Watch Finding Nemo';
+  goals:string[];
+
+  constructor(){
+    this.goals = ['Watch finding Nemo', 'Buy Cookies', 'Get a new phone case']
+
+  }
+
 }
+/*created a new property goals and attributed it to an empty string array.
+Created a constructor function and given the empty goals array 3 string values so it is no longer empty*/
+
